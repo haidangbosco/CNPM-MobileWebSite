561691553fb69218b8b159236d2577ec9eabcb90
Dang Nguyen <darkmatter2411@gmail.com>
2018-05-12 09:29:30 +0700

    Fix urls.py envolving static file

--- MobileSaleWeb/urls.py
+++ MobileSaleWeb/urls.py
@@ -25,5 +25,4 @@ urlpatterns = [
     path('', include('mobile_sale_app.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
++ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
