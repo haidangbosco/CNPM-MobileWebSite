77b4821854cd847f3c92b13a8696d0da9cb6f899
Dang Nguyen <darkmatter2411@gmail.com>
2018-05-22 01:04:42 +0700

    New Version

--- MobileSaleWeb/settings.py
+++ MobileSaleWeb/settings.py
@@ -128,6 +128,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = MEDIA_DIR
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 STATICFILES_DIRS = [
     STATIC_DIR,
     '/var/www/static/',
